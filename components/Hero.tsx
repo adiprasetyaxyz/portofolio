aca20049416ff41ff99c0a843e48056d9c7759ee
unknown <adiprasetyaxyz@icloud.com>
2024-07-28 01:45:18 +0800

    feat: make hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <div className="pt-36 pb-20">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div>
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2] absolute left-0 top-0 flex items-center justify-center">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        </div>
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+              A FullStack Web Developer
+            </h2>
+            <TextGenerateEffect
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              words="Transforming Ideas into Digital Excellence"
+            />
+            <p className="text-center md:tracking-wider my-4 text-sm md:text-xl lg:text-2xl">
+              Hi, Iam Adi Prasetyawan
+            </p>
+            <a href="about">
+              <MagicButton
+                title="Show My Work"
+                icon={<FaLocationArrow />}
+                position="right"
+                otherClasses="items-center"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
