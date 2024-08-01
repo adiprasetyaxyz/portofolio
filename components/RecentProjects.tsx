25465012610e013eb978150fe2a184f7035ded1f
unknown <adiprasetyaxyz@icloud.com>
2024-08-02 01:04:47 +0800

    style: change card style

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import { projects } from "@/data";
 import React from "react";
+import { PinContainer } from "./ui/3d-pin";
+import { FaLocationArrow } from "react-icons/fa";
+import { HoverEffect } from "./ui/CardHover";
 
 export const RecentProjects = () => {
   return (
@@ -9,9 +13,7 @@ export const RecentProjects = () => {
         <span className="text-purple">Recent Projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center gap-16 p-4 mt-10">
-        {projects.map(({ title, id, des, img, iconLists, link }) => (
-          <div key={id}>{title}</div>
-        ))}
+        <HoverEffect items={projects} />
       </div>
     </div>
   );
