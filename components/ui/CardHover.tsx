aa7a456f8a18fb22e6869bec943bd46f05e65fad
unknown <adiprasetyaxyz@icloud.com>
2024-08-02 01:05:17 +0800

    style: change card style

---
+++ components/ui/CardHover.tsx
@@ -0,0 +1,151 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { cn } from "@/utils/cn";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import { useState } from "react";
+import { FaGithub, FaLocationArrow } from "react-icons/fa";
+
+export const HoverEffect = ({
+  items,
+  className,
+}: {
+  items: {
+    title: string;
+    des: string;
+    link: string;
+    img?: string;
+    iconLists: any[];
+    id: number;
+  }[];
+  className?: string;
+}) => {
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-2  py-10 gap-8",
+        className
+      )}
+    >
+      {items.map((item, idx) => (
+        <div
+          key={item?.id}
+          className="relative group  block p-2 h-full w-full max-w-96"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-[#10132E] block  rounded-3xl"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <Card>
+            <img src={item?.img} alt={item?.title} />
+            <div>
+              <CardTitle>{item.title}</CardTitle>
+              <CardDescription>{item.des}</CardDescription>
+            </div>
+            <div className="flex items-center justify-between mt-7 mb-3">
+              <div className="flex items-center pt-4">
+                {item.iconLists.map((icon, index) => (
+                  <div
+                    key={index}
+                    className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                    style={{
+                      transform: `translateX(-${5 * index + 2}px)`,
+                    }}
+                  >
+                    <img src={icon} alt="icon5" className="p-2" />
+                  </div>
+                ))}
+              </div>
+              <div className="flex justify-center items-center gap-2">
+                <Link
+                  href={item.link}
+                  className="mt-3  gap-1 flex justify-center items-center align-middle text-purple text-xs hover:-translate-y-1 transform transition duration-200 hover:shadow-md"
+                >
+                  <p>Web</p>
+                  <FaLocationArrow className="font-medium" color="#CBACF9" />
+                </Link>
+                <Link
+                  href={item.link}
+                  className="mt-3 flex gap-1 justify-center items-center align-middle text-purple text-xs hover:-translate-y-1 transform transition duration-200 hover:shadow-md"
+                >
+                  <p>Github</p>
+                  <FaGithub className="font-medium" color="#CBACF9" />
+                </Link>
+              </div>
+            </div>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const Card = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "rounded-2xl h-full w-full p-4 overflow-hidden  border border-transparent  dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        className
+      )}
+    >
+      <div className="relative z-50">
+        <div className="p-4">{children}</div>
+      </div>
+    </div>
+  );
+};
+export const CardTitle = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
+      {children}
+    </h4>
+  );
+};
+export const CardDescription = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <p
+      className={cn(
+        "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};
